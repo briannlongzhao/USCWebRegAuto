a91bf4065f7c0d67c2b1b002fecab7eb2d0288fc
briannlongzhao <briannlongzhao@gmail.com>
2022-04-15 11:44:25 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # USCWebRegAuto
-Automation script for course registration on USC WebReg system
+
+This is a simple Python script that automates your course registration checkout process on USC WebReg system.
+
+## Dependencies
+
+- [Python 3.x](https://www.python.org/) 
+    Please make sure that pip is installed with Python, and Python directory is added to the `$PATH` environment variable.
+    Open a terminal and type `python --version` and `pip --version`to check installation.
+- [Selenium](https://www.selenium.dev/): 
+    In terminal type `pip install selenium` to install and `pip show selenium` to check installation.
+- [Google Chrome](https://www.google.com/chrome/)
+    Or other available browsers listed on [this page](https://chromedriver.storage.googleapis.com/index.html).
+- [Chrome WebDriver](https://chromedriver.chromium.org/)
+    Please select the correct version corresponding to the version of your browser. Also available on [this page](https://chromedriver.storage.googleapis.com/index.html).
+    Make sure the path of chromedriver executable is added to the `$PATH` environment variable.
+    To test installation, run `chromedriver` in a terminal to see if it can be successfully started.
+    For WebDrivers of other browsers, check [this page](https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/). 
+- [Duo Two-Factor Authentication](https://itservices.usc.edu/duo/)
+    Duo Mobile should be installed on your mobile device and should be able to receive push.
+
+## Run the script
+
+- Login to WebReg system and add the course you want to register into your course bin.
+- Open `script.py` using a text editor and edit line 6-8 with your own myUSC username, email, the term you want to register, and the browser you are using.
+- Open a terminal and change directory into this folder, then type `python script.py` to run the script.
+- Approve the login action on your DUO mobile client.
+- Leave it running to keep monitoring available spots 24/7, or run script just before 9:00 am MWF [when seats of your favorite GE classes will be released](https://dornsife.usc.edu/2015ge/gesm-registration/).
---
+++ script.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+username = ""
+password = "$"
+term = "Fall 2022"
+browser = "Chrome"  # Chrome, Edge, IE, Firefox, or Safari
+
+if browser == "Chrome":
+	options = webdriver.ChromeOptions()
+	driver = webdriver.Chrome(options=options)
+elif browser == "Edge":
+	options = webdriver.EdgeOptions()
+	driver = webdriver.Edge(options=options)
+elif browser == "IE":  # Only runs on Windows
+	options = webdriver.IEOptions()
+	driver = webdriver.Ie(options=options)
+elif browser == "Firefox":
+	options = webdriver.FirefoxOptions()
+	driver = webdriver.Firefox(options=options)
+elif browser == "Safari":  # Only runs on MacOS
+	driver = webdriver.Safari()
+
+wait = WebDriverWait(driver, 180)
+driver.get("https://my.usc.edu")
+wait.until(EC.presence_of_element_located((By.NAME, "j_username"))).send_keys(username)
+wait.until(EC.presence_of_element_located((By.NAME, "j_password"))).send_keys(password)
+driver.find_element(By.CLASS_NAME, "form-button").click()s
+driver.switch_to.frame(driver.find_element(By.ID, "duo_iframe"))
+driver.find_element(By.CLASS_NAME, "push-label").click()
+wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Web Registration"))).click()
+wait.until(EC.number_of_windows_to_be(2))
+for window_handle in driver.window_handles:
+	if window_handle != driver.current_window_handle:
+		driver.switch_to.window(window_handle)
+		break
+wait.until(EC.element_to_be_clickable((By.LINK_TEXT, term))).click()
+while True:
+	wait.until(EC.element_to_be_clickable((By.ID, "mItReg"))).click()
+	wait.until(EC.element_to_be_clickable((By.ID, "SubmitButton"))).click()
+	try:
+		wait.until(EC.text_to_be_present_in_element((By.TAG_NAME, "h3"), "Registration"))
+		response = driver.find_element(By.CSS_SELECTOR, ".content-wrapper-regconfirm").text
+		if ("Failed" in response):
+			continue
+		elif ("successful" in response):
+			print("Success")
+			break
+		else:
+			driver.refresh()
+	except selenium.common.exceptions.TimeoutException:
+		driver.refresh()
+driver.quit()
